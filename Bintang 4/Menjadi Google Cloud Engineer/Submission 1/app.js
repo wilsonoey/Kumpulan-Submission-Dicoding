ca12b2a14c118898163de2a7a8e7e2594b860ff0
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2022-03-16 14:38:19 +0700

    Add files via upload

---
+++ Bintang 4/Menjadi Google Cloud Engineer/Submission 1/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express')
+
+const PORT = 8000
+const HOST = '0.0.0.0'
+
+const app = express()
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/WebProfile.html')
+})
+
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
