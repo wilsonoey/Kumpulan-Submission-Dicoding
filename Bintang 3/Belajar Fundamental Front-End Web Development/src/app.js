f61216e4dd00caebbf0379e481f9322d68916d2c
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2023-11-29 19:00:35 +0800

    Delete Bintang 3 directory

--- Bintang 3/Belajar Fundamental Front-End Web Development/src/app.js
+++
@@ -1,6 +0,0 @@
-import "regenerator-runtime";
-import "./styles/style.css";
-import "./script/component/app-bar.js";
-import main from "./script/view/main.js";
-
-document.addEventListener("DOMContentLoaded", main);
